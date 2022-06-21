9dc2462106a71283ebe18934b66a7fe88bd8233a
saidsn <saidsn@code.edu.az>
2022-06-22 03:01:47 +0400

    StringCode

---
+++ StringCode/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StringCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Letter = { "s", "e", "i", "d" };
+            Word(Letter);
+
+
+
+
+        }
+        static void Word(string[] Letter)
+
+        {
+            int count = 0;
+
+            for (int i = 3; i < Letter.Length; i--)
+            {
+                count++;
+                Console.WriteLine(Letter[i]);
+            }
+        }
+           
+            
+              
+
+        
+            
+        
+    }
+}
